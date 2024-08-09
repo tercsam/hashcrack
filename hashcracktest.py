9fdbab194307cc32f4b166488309196827cce885
tercsam <135607601+tercsam@users.noreply.github.com>
2024-08-09 01:36:11 -0700

    Update hashcracktest.py

--- hashcracktest.py
+++ hashcracktest.py
@@ -12,9 +12,9 @@ def dictionary_attack(hash_to_crack, hash_type, dictionary_file):
         for line in f:
             word = line.strip()
             if hash_password(word, hash_type) == hash_to_crack:
-                print(f"[+] Mot de passe trouvé : {word}")
+                print(f"[+] Password find : {word}")
                 return word
-    print("[-] Mot de passe non trouvé dans le dictionnaire.")
+    print("[-] password not find with dictionnaire.")
     return None
 
 def brute_force_attack(hash_to_crack, hash_type, max_length=4):
@@ -25,22 +25,31 @@ def brute_force_attack(hash_to_crack, hash_type, max_length=4):
             if hash_password(guess, hash_type) == hash_to_crack:
                 print(f"[+] Mot de passe trouvé : {guess}")
                 return guess
-    print("[-] Mot de passe non trouvé par force brute.")
+    print("[-] Password not find with brutforce.")
     return None
 
 def main():
-    hash_to_crack = input("Entrez le hash à cracker : ")
-    hash_type = input("Entrez le type de hash (md5, sha1, sha256, etc.) : ")
-    attack_type = input("Choisissez l'attaque : 'dictionnaire' ou 'brute force' : ").strip().lower()
+    print(r"""
+     _   _           _      ____                _    
+    | | | | __ _ ___| |__  / ___|_ __ __ _  ___| | __
+    | |_| |/ _` / __| '_ \| |   | '__/ _` |/ __| |/ /
+    |  _  | (_| \__ \ | | | |___| | | (_| | (__|   < 
+    |_| |_|\__,_|___/_| |_|\____|_|  \__,_|\___|_|\_\
+
+    """)
+    
+    hash_to_crack = input("Enter the Hash : ")
+    hash_type = input("Enter hash type (md5, sha1, sha256, etc.) : ")
+    attack_type = input("Choose attack type : 'dictionnaire' ou 'brute force' : ").strip().lower()
     
     if attack_type == 'dictionnaire':
-        dictionary_file = input("Entrez le chemin vers le fichier dictionnaire : ")
+        dictionary_file = input("Enter dictionnaire PATH : ")
         dictionary_attack(hash_to_crack, hash_type, dictionary_file)
     elif attack_type == 'brute force':
-        max_length = int(input("Entrez la longueur maximale du mot de passe : "))
+        max_length = int(input("Enter max character numb for password : "))
         brute_force_attack(hash_to_crack, hash_type, max_length)
     else:
-        print("Type d'attaque non reconnu.")
+        print("Attack type not know.")
 
 if __name__ == "__main__":
     main()
